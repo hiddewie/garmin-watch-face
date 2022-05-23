7e3edb4bb1e2e49a4a3526708dafd6d6bf0f9a09
Hidde Wieringa <hidde@hiddewieringa.nl>
2022-05-23 22:06:04 +0200

    PragmataPro Liga Regular font

--- generate-fonts.sh
+++ generate-fonts.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-./fontbm --font-file resources/fonts/Symbola.ttf --chars 48-52 --font-size 48 --output resources/fonts/symbola_48
-./fontbm --font-file resources/fonts/Symbola.ttf --chars 48-57,8586,8587 --font-size 100 --output resources/fonts/symbola_100
-#./fontbm --font-file resources/fonts/Symbola.ttf --chars 48-57,58,8586,8587 --output resources/fonts/symbola_84
+./fontbm --font-file "resources/fonts/PragmataPro Liga Regular.ttf" --chars 48-52 --font-size 48 --output resources/fonts/PragmataPro_48
+./fontbm --font-file "resources/fonts/PragmataPro Liga Regular.ttf" --chars 48-57,8586,8587 --font-size 100 --output resources/fonts/PragmataPro_100
